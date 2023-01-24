5a8321207b1cd42772bdb63503704ad38866ee3f
Jonathan Chng <54364847+JonChng@users.noreply.github.com>
2023-01-24 06:44:56 +0000

    Caesar Cipher Project

---
+++ main.py
@@ -0,0 +1,38 @@
+from art import logo
+
+ok = True
+
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+      end_text += char
+    else:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+while ok:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  if shift > 26:
+    shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  
+  ask = input("Would you like to continue? Y or N:")
+  
+  if ask.upper() == "Y":
+    continue
+  elif ask.upper() == "N":
+    ok = False
